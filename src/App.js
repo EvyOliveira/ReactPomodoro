f18b0efe429ef125b3265457d968c11a3d9bc239
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-02-16 16:38:24 -0300

    feat: contador com 4 ciclos e para no final e cronômetro zera

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { useTimer } from "use-timer";
 import { formatTime } from "./utils/formatTime";
 
 const timerConfig = {
-initialTime : 25,
-timerType : "DECREMENTAL",
-endTime : 0,
-}
+  initialTime: 10,
+  timerType: "DECREMENTAL",
+  endTime: 0
+};
 
 let breaks = 0;
 let completeCycles = 0;
@@ -18,26 +18,20 @@ function App() {
   console.log("Time:", time);
   console.log("Isso vai ser exibido a cada avanço no timer!");
 
-  if(time === 0 && !isBreak) {
-    completeCycles++
-    breaks++
+  if (time === 0 && completeCycles < 3) {
+    breaks++;
+    completeCycles++;
 
-    isBreak = true; 
-
-    if(completeCycles > 1 && completeCycles < 4) 
-    timerConfig.initialTime = 5 && breaks++;
-
-    else {
-      timerConfig.initialTime = 15;
-      breaks = 0;
-    }
+    isBreak = true;
 
     reset();
     start();
+
   }
-  else if(time === 0 && isBreak) {
-    timerConfig.initialTime = 25 - 20;
-      breaks = true;
+  else if(time === 0 && !isBreak && completeCycles ) {
+    timerConfig.initialTime = 5;
+
+    isBreak = false;
 
     reset();
     start();
@@ -46,24 +40,24 @@ function App() {
   return (
     <div>
       <div>
-      <p>Elapsed time: {formatTime(time)}</p>
-      <p>Cycles completed: {completeCycles}</p>
+        <p>Elapsed time: {formatTime(time)}</p>
+        <p>Cycles completed: {completeCycles}</p>
       </div>
-        <button type="button" className="btn btn-outline-success" onClick={start}>
-          Start
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-danger"
-          value={time}
-          onClick={pause}
-        >
-          Pause
-        </button>
-        <button type="button" className="btn btn-outline-primary" onClick={reset}>
-          Reset
-        </button>
-      </div>
+      <button type="button" className="btn btn-outline-success" onClick={start}>
+        Start
+      </button>
+      <button
+        type="button"
+        className="btn btn-outline-danger"
+        value={time}
+        onClick={pause}
+      >
+        Pause
+      </button>
+      <button type="button" className="btn btn-outline-primary" onClick={reset}>
+        Reset
+      </button>
+    </div>
   );
 }
 
