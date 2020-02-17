5e07d0e71540125f60c6cad1a02ca1818914537b
evelynoliveira <evelyn.oliveira@-era.br.com>
2020-02-17 01:17:33 -0300

    refactor: código do funcionamento do relógio

--- src/App.js
+++ src/App.js
@@ -1,51 +1,87 @@
 import React from "react";
 import { useTimer } from "use-timer";
 import { formatTime } from "./utils/formatTime";
+import "./App.css";
 
 const timerConfig = {
-  initialTime: 5,
+  initialTime: 10,
   timerType: "DECREMENTAL",
   endTime: 0
 };
 
-let breaks = 0;
 let completeCycles = 0;
-let isBreak = false;
+let zeroCounter = 0;
+
+let isPause = false;
+let isTimerRunning = false;
+
+let Start = 'Começar';
 
 function App() {
   const { time, start, pause, reset } = useTimer(timerConfig);
 
-  console.log("Time:", time);
-  console.log("Isso vai ser exibido a cada avanço no timer!");
+  if(time === 0)
+  zeroCounter++
 
-  if (time === 0 && !isBreak) {
-    completeCycles++;
-    
-    isBreak = false;
-
+  if(zeroCounter === 2){
     reset();
+    start();
+    zeroCounter = 0;
+    completeCycles++;
+  }
+
+  const startStopTimer = () => {
+    if(isTimerRunning) {
+      isTimerRunning = false;
+      Start = 'Começar';
+      reset();
+      return;
+    }
+
+    isTimerRunning = true;
+    Start = 'Recomeçar';
     start();
   }
 
+  const pauseTimer = () => {
+    if(!isTimerRunning)
+    return;
+
+    if(isPause) {
+      isPause = false;
+      start();
+      return;
+    }
+
+    pause();
+    isPause = true;
+  }
+
+
   return (
-    <div>
+    <div className="app_container" align="center">
       <div>
-        <p>Hello, I'm Pomodoro!</p>
-        <p>Time: {formatTime(time)}</p>
+        <h3>Hello, I'm Pomodoro!</h3>
+        <h1>{formatTime(time)}</h1>
         <p>Cycles completed: {completeCycles}</p>
       </div>
-      <button type="button" className="btn btn-outline-success" onClick={start}>
+      <button
+        className="btn btn-outline-success"
+        type="button"
+        value="Start"
+        onClick={start}
+      >
         Start
       </button>
       <button
-        type="button"
         className="btn btn-outline-danger"
+        type="button"
         value={time}
         onClick={pause}
       >
         Pause
       </button>
-      <button type="button" className="btn btn-outline-primary" onClick={reset}>
+      <button className="btn btn-outline-primary" type="button" onClick={reset}>
         Reset
       </button>
     </div>
